ca8b8b393ebdc4ce680882f810a6b0795a1a878b
GimbleB <david.gimble.barrett@gmail.com>
2016-12-08 11:59:35 +0000

    End of day 08/12/2016

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,22 @@
 /**
  * Created by davem on 27/11/2016.
  */
+'use strict';
 var learnjavascript={};
 
-learnjavascript.showView = function (hash){
-    var questionView =$('<div class="question-view">').text('Arriving soon');
-    $('view-container').empty().append(questionView);
-}
+learnjavascript.questionView=function(problemNumber){
+    var title='Question #'+problemNumber +' Arriving soon!!!';
+    return $('<div class="question-view">').text(title);
+};
+
+
+    learnjavascript.showView = function(hash){
+        var routes = {
+            '#question-1': learnjavascript.questionView
+        };
+        var hashParts = hash.split('-');
+        var viewFn = routes[hashParts[0]];
+        if (viewFn) {
+            $('.view-container').empty().append(viewFn(hashParts[1]));
+        }
+    };
--- tests/appSpec.js
+++ tests/appSpec.js
@@ -9,4 +9,21 @@ describe('LearnJavascript', function(){
 
     });
 
+    it ('shows the landing page view there in not hash',function(){
+       learnjavascript.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    });
+
+    it('passes the hash view parameter to the view function',function(){
+        spyOn(learnjavascript,'questionView');
+        learnjavascript.showView('#question-42');
+        expect(learnjavascript.questionView).toHaveBeenCalledWith('42');
+    });
+
+    describe('problem view', function(){
+        it('has a title that includes the problem number',function(){
+            var view=learnjavascript.questionView('1');
+            expect(view.text()).toEqual('Question #1 Arriving soon!!!');
+        });
+    });
 });
